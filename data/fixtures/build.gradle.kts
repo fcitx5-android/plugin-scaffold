8b0fa5a0f7dec702e28c88d7b1e4f713c4c5c63c
Rocka <i@rocka.me>
2025-07-18 22:49:16 +0800

    Update template

--- data/fixtures/build.gradle.kts
+++ data/fixtures/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-    alias(libs.plugins.android.app) apply false
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.fcitx5.android.app.convention) apply false
     alias(libs.plugins.fcitx5.android.lib.convention) apply false
     alias(libs.plugins.fcitx5.android.plugin.app.convention) apply false
     alias(libs.plugins.fcitx5.android.build.metadata) apply false
-    alias(libs.plugins.fcitx5.android.cmake.dir) apply false
     alias(libs.plugins.fcitx5.android.data.descriptor) apply false
     alias(libs.plugins.fcitx5.android.fcitx.component) apply false
     alias(libs.plugins.fcitx5.android.fcitx.headers) apply false
